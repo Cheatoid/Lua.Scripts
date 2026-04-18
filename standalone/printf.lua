24d5d4285e66ba9e08fe38c3a29c353f16a6ea43
Cheatoid <Cheatoid@users.noreply.github.com>
2026-04-18 12:45:15 +0200

    Add LuaDocs

--- standalone/printf.lua
+++ standalone/printf.lua
@@ -6,6 +6,9 @@
 -- Localized global functions for better performance
 local print = print
 local string_format = string.format
+
+--- Prints a formatted string to the console.
+---@param ... any # Format string and values to format.
 local printf = function(...) return print(string_format(...)) end
 
 -- Export
